46ddaaf9c2ce027492e14b6f6abbdb392701b4d7
AlexanderEnde <alex@endewieanfang.de>
2016-04-07 21:30:44 +0200

    Server.js update.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var mysql   = require("mysql");
 var bodyParser  = require("body-parser");
-var md5 = require('MD5');
+var md5 = require('md5');
 var rest = require("./REST.js");
 var app  = express();
 
