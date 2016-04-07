b9b01451feae46d5fcd79d96fa575577d42190ef
Alexander Ende <alex@localhost.localdomain>
2016-04-07 22:34:35 +0200

    Added passport module.

--- REST.js
+++ REST.js
@@ -6,11 +6,13 @@ function REST_ROUTER(router,connection,md5) {
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
 		connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
-			connection.end();
-			if (!err)
+			connection.release();
+			if (!err) {
 				console.log('The solution is: ', rows);
-			else
+                                res.json(rows); }
+			else {
 				console.log('Error while performing Query.');
+}
 		});
 	});
 }
