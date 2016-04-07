3294c86129c7b2a5894fde3a9c988c16d529ac6c
AlexanderEnde <alex@endewieanfang.de>
2016-04-07 21:48:41 +0200

    REST.js updated and pushed again.

---
+++ REST.js
@@ -0,0 +1,18 @@
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    router.get("/",function(req,res){
+		connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+			connection.end();
+			if (!err)
+				console.log('The solution is: ', rows);
+			else
+				console.log('Error while performing Query.');
+		});
+	});
+}
+
+module.exports = REST_ROUTER;
